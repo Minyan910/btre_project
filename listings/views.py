5a4b576f423299012b86cbeb5fc330552f07fbd0
Minyan910 <minyan.wang@colorado.edu>
2023-08-09 15:42:45 -0600

    finish search

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices, state_choices, bedroom_choices
 
 from .models import Listing
 
@@ -32,5 +33,58 @@ def listing(request, listing_id):
 
     return render(request, 'listings/listing.html', context)
 
+
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    # keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        # note it's square brackets instead of parentheses
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+            # doesn't have to match exactly
+    
+    # city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        # note it's square brackets instead of parentheses
+        if city:
+            queryset_list = queryset_list.filter(city__icontains=city)
+    
+    # state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        # note it's square brackets instead of parentheses
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+            # __iexact is case-insentive
+            # __exact is case-sensitive
+
+    # bedroom
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        # note it's square brackets instead of parentheses
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+            # __iexact is case-insentive
+            # __exact is case-sensitive
+
+    # price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        # note it's square brackets instead of parentheses
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+            # __iexact is case-insentive
+            # __exact is case-sensitive
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+
+    return render(request, 'listings/search.html', context)
