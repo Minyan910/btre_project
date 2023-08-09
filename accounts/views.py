4691dded0853758a4b4bdf6a1663e07b4dc893be
Minyan910 <minyan.wang@colorado.edu>
2023-08-09 16:39:58 -0600

    register/login pages, alerts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        # register user
+        # print("SUBMITTED REG")
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        # login user
+        print("SUBMITTED LOG")
+        return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect(request, 'index')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
